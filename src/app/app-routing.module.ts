b29d9806e68edc2c0c0cbaaeff23a92040be01b9
Alex Medina <afmedina@uniguajira.edu.co>
2023-09-12 08:58:15 -0500

    menu auxiliar para todos los campos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,18 @@ import { ListMultasComponent } from './components/multas/list-multas/list-multas
 import { AddPersonaComponent } from './components/personas/add-persona/add-persona.component';
 import { UpdatePersonaComponent } from './components/personas/update-persona/update-persona.component';
 import { DeletePersonaComponent } from './components/personas/delete-persona/delete-persona.component';
+import { AddDocumentoComponent } from './components/documentos/add-documento/add-documento.component';
+import { UpdateDocumentoComponent } from './components/documentos/update-documento/update-documento.component';
+import { DeleteDocumentoComponent } from './components/documentos/delete-documento/delete-documento.component';
+import { AddMultasComponent } from './components/multas/add-multas/add-multas.component';
+import { UpdateMultasComponent } from './components/multas/update-multas/update-multas.component';
+import { DeleteMultasComponent } from './components/multas/delete-multas/delete-multas.component';
+import { AddVehiculoComponent } from './components/vehiculos/add-vehiculo/add-vehiculo.component';
+import { UpdateVehiculoComponent } from './components/vehiculos/update-vehiculo/update-vehiculo.component';
+import { DeleteVehiculoComponent } from './components/vehiculos/delete-vehiculo/delete-vehiculo.component';
+import { AddInvolucrarComponent } from './components/involucrar/add-involucrar/add-involucrar.component';
+import { UpdateInvolucrarComponent } from './components/involucrar/update-involucrar/update-involucrar.component';
+import { DeleteInvolucrarComponent } from './components/involucrar/delete-involucrar/delete-involucrar.component';
 
 const routes: Routes = [
   {
@@ -39,8 +51,32 @@ const routes: Routes = [
     component: ListDocumentoComponent
   },
   {
+    path: 'documentos/add',
+    component: AddDocumentoComponent
+  },
+  {
+    path: 'documentos/update',
+    component: UpdateDocumentoComponent
+  },
+  {
+    path: 'documentos/delete',
+    component: DeleteDocumentoComponent
+  },
+  {
     path: 'vehiculos',
     component: ListVehiculoComponent
+  },
+  {
+    path: 'vehiculos/add',
+    component: AddVehiculoComponent
+  },
+  {
+    path: 'vehiculos/update',
+    component: UpdateVehiculoComponent
+  },
+  {
+    path: 'vehiculos/delete',
+    component: DeleteVehiculoComponent
   },
 
   {
@@ -49,8 +85,35 @@ const routes: Routes = [
   },
 
   {
+    path: 'involucrar/add',
+    component: AddInvolucrarComponent
+  },
+
+  {
+    path: 'involucrar/update',
+    component: UpdateInvolucrarComponent
+  },
+
+  {
+    path: 'involucrar/delete',
+    component: DeleteInvolucrarComponent
+  },
+
+  {
     path: 'multas',
     component: ListMultasComponent
+  },
+  {
+    path: 'multas/add',
+    component: AddMultasComponent
+  },
+  {
+    path: 'multas/update',
+    component: UpdateMultasComponent
+  },
+  {
+    path: 'multas/delete',
+    component: DeleteMultasComponent
   },
 
 ];
